b46ec9a0ddca4961904deadcb448e8884f918cb4
Au Au <18813102633@163.com>
2019-07-18 17:50:16 +0800

    init

---
+++ faker.py
@@ -0,0 +1,41 @@
+import docx
+
+
+def docx_faker(size=1024):
+    """Parameters:
+        size: the size of the documents u want (KB).
+    Returns: None
+    Results: generate a fake file with the format of docx.
+    """
+    pass
+
+
+def xlsx_faker(size=1024):
+    """Parameters:
+        size: the size of the documents u want (KB).
+    Returns: None
+    Results: generate a fake file with the format of xlsx.
+    """
+    pass
+
+
+def pptx_faker(size=1024):
+    """Parameters:
+        size: the size of the documents u want (KB).
+    Returns: None
+    Results: generate a fake file with the format of pptx.
+    """
+    pass
+
+
+def pdf_faker(size=1024):
+    """Parameters:
+        size: the size of the documents u want (KB).
+    Returns: None
+    Results: generate a fake file with the format of pdf.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
